a7f38f3e94fd7bf4e55141ab43e23723a173d23c
JakubKivi <panmcleon@gmail.com>
2018-04-20 22:38:04 +0200

    Zmiana pinów na płytkę Kiviego

--- code/sumo/header.h
+++ code/sumo/header.h
@@ -1,30 +1,34 @@
 
 
 //floor sensor
-#define floorFL 13 	//front left 
-#define floorFR	4	//front right
-#define floorBL	1	//back
-#define floorBR 3
+#define floorFL A0 	//front left 
+#define floorFR	A1	//front right
+#define floorBL	A2	//back
+#define floorBR A3
 //distance sensor front
-#define sensorF A1	
-#define sensorFL 13
-#define sensorFR A0
-#define sensorB A3
-#define sensorBL A2
-#define sensorBR 0
+#define sensorFR 3
+#define sensorBR 4
+#define sensorB 2
+#define sensorBL 1
+#define sensorFL 7
+#define sensorF 0
 
 //motor
-#define AIN1 7
-#define AIN2 6       //na razie a to Lewy b to Prawy 1 to przód 2 tył
-#define BIN1 9
-#define	BIN2 10
-#define PWMA 5 
-#define PWMB 11
-#define STBY 8
+#define AIN1 9
+#define AIN2 10
+#define BIN1 12
+#define	BIN2 13
+#define PWMA 8 
+#define PWMB 7
 
 bool direction = true;
 int speed; 
-int d=50;         //============== 50 jest chwilowe potem ustalimy na pods testów ======================//
+int d=50;         //=== 50 jest chwilowe potem ustalimy na pods testów ===\\
+
+bool check(int pin){
+  if (analogRead(pin)>10) return 1;
+  else return 0;
+}
 
 //sterowanie silnika
 void goForward(){
@@ -48,7 +52,7 @@ void goForwardLeft(){
   }
   
 void goForwardRight(){
-  digitalWrite(AIN1, HIGH);
+  digitalWrite(AIN1, LOW);
   digitalWrite(AIN2, LOW);
   digitalWrite(BIN1, HIGH);
   digitalWrite(BIN2, LOW);
@@ -68,14 +72,14 @@ void goBackword(){
 void goBackwardLeft(){
   digitalWrite(AIN2, HIGH);
   digitalWrite(AIN1, LOW);
-  digitalWrite(BIN2, HIGH);
+  digitalWrite(BIN2, LOW);
   digitalWrite(BIN1, LOW);
   
   analogWrite(PWMA, speed-d);  
   analogWrite(PWMB, speed);
   }
 void goBackwardRight(){
-  digitalWrite(AIN2, HIGH);
+  digitalWrite(AIN2, LOW);
   digitalWrite(AIN1, LOW);
   digitalWrite(BIN2, HIGH);
   digitalWrite(BIN1, LOW);
@@ -89,13 +93,13 @@ void goRandomly(){
     digitalWrite(AIN1, HIGH);
     digitalWrite(AIN2, LOW);
     digitalWrite(BIN1, HIGH);
-    digitalWrite(BIN2, LOW);
+    digitalWrite(BIN2, LOW);   //jade w przód
     if(vector){
         analogWrite(PWMB, speed-d);
-        analogWrite(PWMA, speed);
+        analogWrite(PWMA, speed); //ale skręcam trochę w lewo
     }else{
       analogWrite(PWMB, speed);
-      analogWrite(PWMA, speed-d);
+      analogWrite(PWMA, speed-d);   //albo troche w prawo
     }
   }else{
     digitalWrite(AIN2, HIGH);
@@ -132,15 +136,15 @@ void go(){   //jedź just jedź
 }
 
 bool touchFloor(){                //czy dotyka podłogi
-  return  digitalRead(floorFL)  ||
-      digitalRead(floorFR)  ||
-      digitalRead(floorBR)  ||
-      digitalRead(floorBL);
+  return  check(floorFL)  ||
+      check(floorFR)  ||
+      check(floorBR)  ||
+      check(floorBL);
 }
 
 bool seeEnemy(){                  //czy widzi przeciwnika
   return  digitalRead(sensorF)  ||
-      digitalRead(sensorFL)     ||
+      check(sensorFL)     ||
       digitalRead(sensorFR)     ||
       digitalRead(sensorB)      ||
       digitalRead(sensorBL)     ||
@@ -166,12 +170,12 @@ void toSeeEnemyFront(){              //zrób tak żeby przeciwnik był nawprost
 }
 
 void outOfLine(){
-     if(digitalRead(floorFL)^digitalRead(floorBL)^digitalRead(floorFR)^digitalRead(floorBR)){
-        if(digitalRead(floorFL)) goBackwardRight();
-        if(digitalRead(floorFR)) goBackwardLeft();
-        if(digitalRead(floorBL)) goForwardRight();
-        if(digitalRead(floorBR)) goForwardLeft();
-     }else if(digitalRead(floorFL)&&digitalRead(floorBL)){
+     if(check(floorFL)^check(floorBL)^check(floorFR)^check(floorBR)){
+        if(check(floorFL)) goBackwardRight();
+        if(check(floorFR)) goBackwardLeft();
+        if(check(floorBL)) goForwardRight();
+        if(check(floorBR)) goForwardLeft();
+     }else if(check(floorFL)&&check(floorBL)){
       if(seeEnemy){
         
       }else{
@@ -185,8 +189,8 @@ void outOfLine(){
       }
       
     }
-    else if(digitalRead(floorFL)&&digitalRead(floorFR)) goBackword();
-    else if(digitalRead(floorBR)&&digitalRead(floorBL)) goForward();
+    else if(check(floorFL)&&check(floorFR)) goBackword();
+    else if(check(floorBR)&&check(floorBL)) goForward();
     else{ 
       digitalWrite(AIN2, HIGH);
       digitalWrite(AIN1, LOW);
